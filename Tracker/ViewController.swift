dfa65242991c95913645dbad164764a00aa8e965
lithone <wkadosh@gmail.com>
2016-12-31 10:56:25 -0500

    Initial Commit

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Warren Kadosh on 2016-12-31.
+//  Copyright © 2016 Warren Kadosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
